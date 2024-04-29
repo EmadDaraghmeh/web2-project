4c792bc98a9a223c93ad8d9343bd963875bc0e45
EmadDaraghmeh <s12116485@stu.najah.edu>
2024-04-29 23:53:04 +0300

    the Home page was added with the navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,16 @@
-import BrandP from "./Components/BrandProfile/BrandP";
-import SignUP from "./Components/SignUp2/SignUp";
+// import BrandP from "./Components/BrandProfile/BrandP";
+// import SignUP from "./Components/SignUp2/SignUp";
+
+import InfluencerHome from "./Components/LoadingInfluencerPage/InfluencerHome";
+import InfluencerNavbar from "./Components/LoadingInfluencerPage/InfluencerNavbar";
 
 function App() {
-  return <SignUP />;
-}
+  return( 
+  // <SignUP />
+  <div className="app">
+    <InfluencerNavbar/>
+    <InfluencerHome/>
+  </div>
+)}
 
 export default App;
