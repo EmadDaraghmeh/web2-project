e5df7422dc7971edf94fef29e6e7e3cb3cfe1d9d
omar <omar.hajdaoud8@gmail.com>
2024-05-02 14:33:25 +0300

    i add new page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,18 @@
-// import BrandP from "./Components/BrandProfile/BrandP";
+import BrandP from "./Components/BrandProfile/BrandP";
 // import SignUP from "./Components/SignUp2/SignUp";
 
 // import InfluencerHome from "./Components/LoadingInfluencerPage/InfluencerHome";
 // import InfluencerNavbar from "./Components/LoadingInfluencerPage/InfluencerNavbar";
-import MainLandingPage from "./Components/MainLandingPage/MainLandingPage";
 
 function App() {
-  return( 
-  // <SignUP />
-  <div className="app">
-    <MainLandingPage></MainLandingPage>
-  </div>
-)}
+  return (
+    // <SignUP />
+    <div className="app">
+      {/* <InfluencerNavbar/>
+    <InfluencerHome/> */}
+      <BrandP />
+    </div>
+  );
+}
 
 export default App;
