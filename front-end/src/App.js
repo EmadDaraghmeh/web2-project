c1a8d3fb4b100df44058d7809d682d175f4efa4f
EmadDaraghmeh <s12116485@stu.najah.edu>
2024-05-02 14:49:49 +0300

    the back-end folder was readd, i also add the express package

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,15 @@
 // import BrandP from "./Components/BrandProfile/BrandP";
 // import SignUP from "./Components/SignUp2/SignUp";
 
-// import InfluencerHome from "./Components/LoadingInfluencerPage/InfluencerHome";
-// import InfluencerNavbar from "./Components/LoadingInfluencerPage/InfluencerNavbar";
-import MainLandingPage from "./Components/MainLandingPage/MainLandingPage";
+import InfluencerHome from "./Components/LoadingInfluencerPage/InfluencerHome";
+import InfluencerNavbar from "./Components/LoadingInfluencerPage/InfluencerNavbar";
+
 
 function App() {
   return( 
   // <SignUP />
   <div className="app">
-    <MainLandingPage></MainLandingPage>
+    <InfluencerNavbar/>
   </div>
 )}
 
