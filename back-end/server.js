74b7aa00c8d005616224b1dd3f7f1119ba7852cc
EmadDaraghmeh <s12116485@stu.najah.edu>
2024-05-27 10:22:58 +0300

    i add the user schema

--- back-end/server.js
+++ back-end/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { PORT, mongoDBURL } = require("./config");
 const SignUpInfluencer=require('./src/Controller/SignUpInfluencerRouter')
+const path=require('path')
 const app = express();
 
 // console.log(typeof BrandModel)
@@ -27,11 +28,11 @@ app.use("/brands", routerBlog);
 app.use("/SignUpInfluencerRouter",SignUpInfluencer);
 app.get("*", (req, res) => {
 	console.log(req);
-	return res.status(404).send("Hello");
+	return res.status(404).send(path.dirname(__filename));
 });
 
 mongoose
-	.connect(mongoDBURL)
+	.connect(mongoDBURL,{useNewUrlParser: true,useUnifiedTopology: true})
 	.then(() => {
 		console.log("App connected to database");
 		app.listen(PORT, () => {
