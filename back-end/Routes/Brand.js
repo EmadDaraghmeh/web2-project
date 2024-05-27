2e5e67daebda66716bcfedfc71834db0d2970412
EmadDaraghmeh <s12116485@stu.najah.edu>
2024-05-27 09:15:02 +0300

    routes was deleted

--- back-end/Routes/Brand.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const brandRouter=express.Router()
-const brandController=require('../Controller/CreateNewBrand')
-brandRouter.post('/',brandController.createBrand)
-brandRouter.get('/:id',brandController.getBrand)
-brandRouter.get('/',brandController.getAllBrands)
-
-module.exports=brandRouter;
