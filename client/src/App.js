361d4a4c24fdca59a0913f88cf4774ced2eb4ce1
EmadDaraghmeh <s12116485@stu.najah.edu>
2024-05-30 21:48:00 +0300

    the sidebar was deleted from the app

--- client/src/App.js
+++ client/src/App.js
@@ -225,7 +225,7 @@ function App() {
 								<div>
 									<InfluencerNavbar />
 									<div className="sidebar">
-										<SideBar />
+										
 										<SearchBrand />
 									</div>
 									<Footer />
