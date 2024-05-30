00253453be5feda432b2087ede5c5dc5a0dbe632
Bushra <boshra.o.hourani@gmail.com>
2024-05-30 20:22:02 +0300

    message

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routerBlog =require('./src/Controller/BlogPostsRouter')
-const cors=require('cors')
-const router=require('./src/Controller/backend/routes/BlogRoutes')
-const { PORT, mongoDBURL } = require('./config'); 
-const app = express();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const routerBlog =require('./src/Controller/BlogPostsRouter')
+// const cors=require('cors')
+// const router=require('./src/Controller/backend/routes/BlogRoutes')
+// const { PORT, mongoDBURL } = require('./config'); 
+// const app = express();
 
-// console.log(typeof BrandModel)
-app.use(express.json())
-app.use(cors())
+// // console.log(typeof BrandModel)
+// app.use(express.json())
+// app.use(cors())
 
-app.get('/',(req,res)=>{
-    console.log(req)
-    return res.status(234).send('Hello')
-})
+// app.get('/',(req,res)=>{
+//     console.log(req)
+//     return res.status(234).send('Hello')
+// })
 
-app.use('/brands',routerBlog)
-app.use('/blog',router)
-mongoose.connect(mongoDBURL).then(() => {
-    console.log('App connected to database');
-    app.listen(PORT, () => {
-        console.log(`server running on port ${PORT}`);
-        console.log(`http://localhost:${PORT}`);
-    });
-}).catch((error) => {
-    console.log(error);
-});
+// app.use('/brands',routerBlog)
+// app.use('/blog',router)
+// mongoose.connect(mongoDBURL).then(() => {
+//     console.log('App connected to database');
+//     app.listen(PORT, () => {
+//         console.log(`server running on port ${PORT}`);
+//         console.log(`http://localhost:${PORT}`);
+//     });
+// }).catch((error) => {
+//     console.log(error);
+// });
